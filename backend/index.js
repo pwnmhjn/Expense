590dffd5b48947ff2a485724c1d74d40c19ffd9e
pwnmhjn <pwnmhjn22@gmail.com>
2024-12-07 19:17:39 +0530

    password connfig

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,46 @@
 import express from "express"
 import http from "http"
 import cors from "cors"
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import passport from "passport";
+import session from "express-session";
 import { expressMiddleware } from "@apollo/server/express4";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { buildContext } from "graphql-passport";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import connectMongo from "connect-mongodb-session";
+import { ApolloServer } from "@apollo/server";
+
+
 import mergedResolver from "./resolvers/index.js";
 import mergedTypeDefs from "./TypeDefs/index.js"
 import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
 const app = express()
 
 const httpServer = http.createServer(app)
+
+const MongoDbStore = connectMongo(session)
+
+const store = new MongoDbStore({
+    uri: process.env.MONGO_URL,
+    collection: "session"
+})
+
+store.on("error", (err) => console.log(err))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    },
+    store: store
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const server = new ApolloServer({
     typeDefs: mergedTypeDefs,
@@ -22,10 +52,13 @@ await server.start()
 
 app.use(
     '/',
-    cors(),
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }),
     express.json(),
     expressMiddleware(server, {
-        context: async ({ req }) => (req),
+        context: async ({ req, res }) => buildContext({ req, res }),
     }),
 );
 
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -2,10 +2,10 @@ import { users } from "../dummyData/Data.js"
 
 const userResolver = {
     Query: {
-        users: () => {
+        users: (_, __, { req, res }) => {
             return users
         },
-        user: (p_, { userId }) => {
+        user: (_, { userId }) => {
             return users.find((user) => user._id === userId)
         }
     },
